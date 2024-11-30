cb77e1f174d44bfb958ce9b066bd393a67b9adad
Mohammed Minhaj <nijulinux@gmail.com>
2024-11-30 13:50:25 +0530

    fetched data

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,36 @@
+import {Component, DestroyRef, inject, signal} from '@angular/core';
+import  { map } from 'rxjs'
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent{
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destoryRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.httpClient.get<{ places: Place[ ] }>(`http://localhost:3000/places`)
+      .pipe(
+        map((resData) => resData.places)
+      )
+      .subscribe({
+      next: places => {
+        this.places.set(places);
+      }
+    })
+
+    this.destoryRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+}
